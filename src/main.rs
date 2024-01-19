ca5cf69ed774298dad8acb03ab62469fd911dcb7
Aviion Gibbs <xanthus58@protonmail.com>
2024-01-19 14:20:01 +1030

    Add: db intergration

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,67 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use rand::Rng;
+use actix_web::{dev::Response, get, post, web, App, HttpResponse, HttpServer, Responder};
+use sqlite::State;
+
+fn returndbdata(creature: &str) -> String {
+    let connection = sqlite::open("./sqlite.db").unwrap();
+    let mut query = "SELECT * FROM cosdata WHERE creature = ";
+
+    let creature = format!("'{}'", creature);
+
+    let binding = (query.to_string() + &creature);
+    query = &binding;
+
+    let mut statement = connection.prepare(query).unwrap();
+
+
+    let mut response = String::new();
+    while let Ok(State::Row) = statement.next() {
+        let creature = statement.read::<String, _>("creature").unwrap();
+        let price = statement.read::<String, _>("price").unwrap();
+        response = creature + ", " + &price.to_string();
+        println!("{}", response);
+    }
+    return response.to_string().to_lowercase();
+}
 
 #[get("/")]
 async fn root() -> impl Responder {
-    HttpResponse::Ok().body("Endpoints:\nAdharcaiin\nAereis\nAesho")
+    HttpResponse::Ok().body("Endpoints:\n    Adharcaiin\n    Aereis\n    Aesho")
 }
 
 #[get("/Adharcaiin")]
 async fn Adharcaiin() -> impl Responder {
-    HttpResponse::Ok().body("Creature: Adharcaiin \nPrice: $1,000 mush")
+    let response = returndbdata("Adharcaiin");
+    HttpResponse::Ok().body(response)
 }
 
 #[get("/Aereis")]
 async fn Aereis() -> impl Responder {
-    HttpResponse::Ok().body("Creature: Aereis \nPrice: $25,000-$30,000 mush")
+    let response = returndbdata("Aereis");
+    HttpResponse::Ok().body(response)
 }
 
 #[get("/Aesho")]
 async fn Aesho() -> impl Responder {
-    HttpResponse::Ok().body("Creature: Aesho \nPrice: $50 mush")
+    let response = returndbdata("Aesho");
+    HttpResponse::Ok().body(response)
 }
-
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let creatures = ["Adharcaiin", "Aereis", "Aesho"];
+
+    let connection = sqlite::open("./sqlite.db").unwrap();
+        // Query string formatted in SQL
+        let query = "
+        CREATE TABLE cosdata (creature TEXT, price INTEGER);
+        INSERT INTO cosdata VALUES ('Adharcaiin', 1000);
+        INSERT INTO cosdata VALUES ('Aereis', 25000);
+        INSERT INTO cosdata VALUES ('Aesho', 50);
+    ";
+
+    // Calls the query to be ran in the database
+    connection.execute(query).unwrap();
+
     HttpServer::new(|| {
         App::new()
             .service(root)
@@ -35,4 +73,3 @@ async fn main() -> std::io::Result<()> {
     .run()
     .await
 }
-
